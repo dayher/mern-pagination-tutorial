7a6b3b18365ad1df1503f7d9d66c60c5fabb6b09
Dayher Marchan <dayhermarchan@gmail.com>
2021-12-13 10:52:18 -0300

    Final deployment prep

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const connectDB = require("./config/db");
 const postRoutes = require("./routes/postRoutes");
+const path = require("path");
 
 connectDB();
 
@@ -10,6 +11,17 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/v1/posts", postRoutes);
+
+if(process.env.MODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,'client/build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname,"client","build","index.html"));
+    });
+}else{
+    app.get("/", (req, res)=>{
+        res.send("Api running");
+    });
+}
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
